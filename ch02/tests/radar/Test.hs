f9d9786fdbf05d935dc3b6a3f118f8f40734192b
akthrms <akt.hrms@gmail.com>
2022-05-30 22:56:23 +0900

    update

--- ch02/tests/radar/Test.hs
+++ ch02/tests/radar/Test.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE StandaloneDeriving #-}
 
 import qualified Control.Monad as Monad
+import qualified Data.List as List
 import Flow ((<|))
-import Radar (Direction (..), Turn (..))
+import Radar (Direction (..), Turn (..), every, orient, orientMany, rotateManySteps)
+import qualified System.Exit as Exit
 import qualified System.Random as Random
 import qualified System.Random.Stateful as RandomS
 
@@ -49,3 +51,31 @@ writeRandomFile n generator filename =
   do
     xs <- generator n
     writeFile filename <| unlines <| map show xs
+
+test_allTurnsInUse :: Bool
+test_allTurnsInUse =
+  List.sort
+    ( List.nub
+        [ orient direction1 direction2 | direction1 <- every, direction2 <- every
+        ]
+    )
+    == every
+
+test_orientRotateAgree :: [Direction] -> Bool
+test_orientRotateAgree directions =
+  case directions of
+    (direction : _) ->
+      directions == rotateManySteps direction (orientMany directions)
+    [] ->
+      True
+
+main :: IO ()
+main =
+  do
+    directions <- randomDirections 1000
+    -- turns <- randomTurns 1000
+    Monad.when
+      ( not
+          <| test_allTurnsInUse && test_orientRotateAgree directions
+      )
+      Exit.exitFailure
