772f265842eaf180a3750c116164c7f9bbb93bd1
akthrms <akt.hrms@gmail.com>
2022-06-01 11:40:18 +0900

    update

---
+++ ch02/app/Contexts.hs
@@ -0,0 +1,25 @@
+module Contexts where
+
+import qualified Control.Monad.Writer as Writer
+import Flow ((<|))
+
+readNumber :: IO Int
+readNumber =
+  do
+    s <- getLine
+    pure <| read s
+
+sumN :: Int -> Writer.Writer String Int
+sumN 0 = Writer.writer (0, "finish")
+sumN n =
+  do
+    Writer.tell <| show n ++ ","
+    s <- sumN (n - 1)
+    pure <| n + s
+
+cartesianProduct :: [Int] -> [Int] -> [(Int, Int)]
+cartesianProduct xs ys =
+  do
+    x <- xs
+    y <- ys
+    pure <| (x, y)
