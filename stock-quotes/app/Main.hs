36f2d240fbabdf16f6c338d49aef0b028073fdf9
akthrms <akt.hrms@gmail.com>
2022-06-05 19:24:51 +0900

    update

--- stock-quotes/app/Main.hs
+++ stock-quotes/app/Main.hs
@@ -1,4 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
+import Charts
+import qualified Control.Monad as Monad
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Csv as Csv
+import qualified Data.Text as Text
+import Flow ((<|))
+import HtmlReport
+import Params
+import QuoteData
+import StatReport
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = cmdLineParser >>= work
+
+work :: Params -> IO ()
+work params =
+  do
+    csvData <- BS.readFile (filename params)
+    case Csv.decodeByName csvData of
+      Left err -> putStrLn err
+      Right (_, quotes) -> generateReports params quotes
+
+generateReports :: (Functor t, Foldable t) => Params -> t QuoteData -> IO ()
+generateReports Params {..} quotes =
+  do
+    Monad.unless silent <| putStrLn textReport'
+    Monad.when chart <| plotChart title quotes chartFilename
+    saveHtml htmlFile htmlReport'
+  where
+    statInfo' =
+      statInfo quotes
+
+    textReport' =
+      textReport statInfo'
+
+    htmlReport' =
+      htmlReport title quotes statInfo' [chartFilename | chart]
+
+    withCompany prefix =
+      maybe mempty (prefix <>) company
+
+    chartFilename =
+      Text.unpack <| "chart" <> withCompany "_" <> ".svg"
+
+    title =
+      Text.unpack <| "Historical Quotes" <> withCompany " for "
+
+    saveHtml Nothing _ = pure ()
+    saveHtml (Just f) html = BS.writeFile f html
