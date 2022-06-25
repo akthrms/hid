bcf4a68e60841551eba810bb6ac36b40bb7ffba1
akthrms <akt.hrms@gmail.com>
2022-06-25 22:43:39 +0900

    update

---
+++ ch05/app/Reader.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Reader where
+
+import Control.Monad.Reader (Reader, when)
+import qualified Control.Monad.Reader as R
+
+newtype Config = Config {verbose :: Bool}
+
+getConfiguration :: IO Config
+getConfiguration =
+  pure Config {verbose = True}
+
+type ConfigM = Reader Config
+
+work :: ConfigM ()
+work =
+  do
+    doSomething
+
+doSomething :: ConfigM ()
+doSomething =
+  do
+    doSomethingSpecial
+
+doSomethingSpecial :: ConfigM ()
+doSomethingSpecial =
+  do
+    Config {verbose} <- R.ask
+    when verbose beVerbose
+
+beVerbose :: ConfigM ()
+beVerbose =
+  pure ()
+
+silent :: Config -> Config
+silent config =
+  config {verbose = False}
+
+doSomethingSpecialSilently :: ConfigM ()
+doSomethingSpecialSilently =
+  R.local silent doSomethingSpecial
+
+main :: IO ()
+main =
+  do
+    config <- getConfiguration
+    let result = R.runReader work config
+    print result
